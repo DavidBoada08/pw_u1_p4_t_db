9cfc6815b601bc48bf15f55e966620e5294f4b60
DavidBoada08 <dfboadaa@uce.edu.ec>
2023-05-15 17:48:27 -0500

    Mi Primer deber

---
+++ src/main/java/com/example/tradicional/PwU1P4TDbApplication.java
@@ -0,0 +1,22 @@
+package com.example.tradicional;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwU1P4TDbApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwU1P4TDbApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Mi Primera tarea");
+		
+	}
+
+}
---
+++ src/test/java/com/example/tradicional/PwU1P4TDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tradicional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwU1P4TDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
